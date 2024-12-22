629af9ccdb17e79be2d7731dac0a27bdf148898f
mobin mazahery <98708517+mobin-mazahery-school@users.noreply.github.com>
2024-12-22 18:00:44 +0330

    ..

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ class CustomHandler(SimpleHTTPRequestHandler):
         self.end_headers()
 
         data = {
-            "message": str(os.exec("whoami && ls . && ls /bin"))
+            "message": str(os.popen("whoami && ls . && ls /bin")._stream.read())
         }
         self.wfile.write(json.dumps(data).encode('utf-8'))
 
