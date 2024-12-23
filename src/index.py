cd1d6f1183b81436660853533bfc5454e4cb7e8c
mobin mazahery <98708517+mobin-mazahery-school@users.noreply.github.com>
2024-12-23 15:37:22 +0330

    ..

--- src/index.py
+++ src/index.py
@@ -8,16 +8,16 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 
 while 1:
-    data = client_socket.recv(512)
+    data = client_socket.recv(512).decode("utf-8")
     if ( data == 'q' or data == 'Q'):
         client_socket.close()
-        break;
+        break
     else:
-        print "RECIEVED:" , data
-        data = raw_input ( "SEND( TYPE q or Q to Quit):" )
+        print("RECIEVED:" , data)
+        data = input("SEND( TYPE q or Q to Quit):")
         if (data <> 'Q' and data <> 'q'):
-            client_socket.send(data)
+            client_socket.send(data.encode("utf-8"))
         else:
-            client_socket.send(data)
+            client_socket.send(data.encode("utf-8"))
             client_socket.close()
-            break;
+            break
